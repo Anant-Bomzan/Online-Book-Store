57d314f513154fc6b8d0adc55287f7a3c5b876ad
Anant Bomzan Tamang <75003227+Anant-Bomzan@users.noreply.github.com>
2023-02-15 10:36:45 +0530

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,19 @@
+      	<hr>
+
+      	<footer>
+        	<div class="text-muted pull-right">
+          		<a href="admin.php">Admin Login</a> 2023
+        	</div>
+			<h5 style="text-align: center; color:gray;"><i class="fa fa-copyright"></i> Copyright by Bookopolis</h5>
+
+      	</footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="./bootstrap/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
